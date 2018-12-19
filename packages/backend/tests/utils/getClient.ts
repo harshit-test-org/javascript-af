50c8dcc4fdb66609ddbbed70e986f906a59374a9
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-12-19 16:48:51 +0100

    test: dynamic client uri

--- packages/backend/tests/utils/getClient.ts
+++ packages/backend/tests/utils/getClient.ts
@@ -1,9 +1,12 @@
 import ApolloBoost from 'apollo-boost';
 
+const port = process.env.PORT || 4000;
+const { graphqlPath } = require('../../src/apolloServer');
+
 const getClient = () => {
-    return new ApolloBoost({
-        uri: `http://localhost:4000/graphql`
-    });
+  return new ApolloBoost({
+    uri: `http://localhost:${port}${graphqlPath}`,
+  });
 };
 
-export { getClient }
+export { getClient };

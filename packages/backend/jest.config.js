7a09b3228502c963914749eaafb86d43f1340bec
Harshit Pant <pantharshit00@gmail.com>
2018-12-21 00:06:37 +0530

    fix: make us 100% ts again

--- packages/backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  globalSetup: './tests/utils/globalSetup.ts',
-  globalTeardown: './tests/utils/globalTeardown.ts',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};

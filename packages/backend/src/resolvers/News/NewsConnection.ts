3c0da4fa4a431c2ce1b04973d98b90ddb4dcb97e
Harshit Pant <pantharshit00@gmail.com>
2018-12-22 13:16:08 +0530

    feat: add user nodeitems resolution and remove autogenerated comments

--- packages/backend/src/resolvers/News/NewsConnection.ts
+++ packages/backend/src/resolvers/News/NewsConnection.ts
@@ -1,6 +1,3 @@
-// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
-// Please do not import this file directly but copy & paste to your application code.
-
 import { NewsConnectionResolvers } from '../../generated/graphqlgen';
 
 export const NewsConnection: NewsConnectionResolvers.Type = {

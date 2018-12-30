e4c70f519343dc304ad816acf23a5f70f7620d4f
Harshit Pant <pantharshit00@gmail.com>
2018-12-30 23:10:30 +0530

    fix: remove docz folder

--- packages/frontend/.docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'components/Button/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "components-button-index" */ 'components/Button/index.mdx'),
-}

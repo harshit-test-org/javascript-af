d8c0cb3ae296585a662440361f2816c7537f993e
Nicky Meuleman <nicky.meuleman@gmail.com>
2019-01-02 19:33:12 +0100

    build(test): init cypress infrauses cypress-testing-library

--- packages/frontend/components/ForTest.tsx
+++ packages/frontend/components/ForTest.tsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class ForTest extends Component {
   render() {
-    return <div>hello jsaf</div>;
+    return <div data-testid="for-test">hello jsaf</div>;
   }
 }
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import ForTest from '../components/ForTest';
 
 export default () => {
   const [state, setCount] = useState(0);
-  return <button onClick={() => setCount(state + 1)}>{state}</button>;
+  return (
+    <div>
+      <ForTest />
+      <button onClick={() => setCount(state + 1)}>{state}</button>
+    </div>
+  );
 };

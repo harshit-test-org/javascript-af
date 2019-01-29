6adfd7d194369cb85b4e32920bb73f9d137da9ad
Nicky Meuleman <nicky.meuleman@gmail.com>
2019-01-30 00:36:52 +0100

    refactor(tests): replace testframeworksetup with setupfilesafterenv
    setupTestFrameworkScriptFile was deprecated https://github.com/facebook/jest/pull/7119

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -2,7 +2,11 @@ const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$';
 
 module.exports = {
   testRegex: TEST_REGEX,
-  setupTestFrameworkScriptFile: '<rootDir>/testUtils/setupTest.ts',
+  setupFilesAfterEnv: [
+    'jest-dom/extend-expect',
+    'react-testing-library/cleanup-after-each',
+    'jest-styled-components',
+  ],
   transform: {
     '^.+\\.tsx?$': 'babel-jest',
   },

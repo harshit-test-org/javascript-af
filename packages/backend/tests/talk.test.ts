d49ea9ce103efb17c16fed2654fcdb240fb2af4b
Harshit Pant <pantharshit00@gmail.com>
2019-02-14 23:19:48 +0530

    fix: mock implementation of prisma

--- packages/backend/tests/talk.test.ts
+++ packages/backend/tests/talk.test.ts
@@ -1,75 +1,36 @@
 import { graphqlTestCall } from './utils/gqlTestClient';
 import { prisma } from './utils/createPrismaMock';
-import { aggregateMock, speakerMock, tagsMock } from './mocks';
+import { aggregateMock, speakerMock } from './mocks';
 
 describe('Talk Resolvers', () => {
-  prisma.talksConnection = jest.fn(() => ({
-    pageInfo: {
-      hasNextPage: false,
-      hasPreviousPage: false,
-      startCursor: 'cjq0qx0v800050981rha92aq1',
-      endCursor: 'cjq0qx0v800050981rha92aq1',
-    },
-    edges: [
-      {
-        node: {
-          id: 'cjq0qx0v800050981rha92aq1',
-          title: 'Prisma: ORM to rule em all',
-        },
-        cursor: 'cjq0qx0v800050981rha92aq1',
-      },
-      {
-        node: {
-          id: 'cjq0qx0v800050981rha92qsd',
-          title: 'ORM ORM ORM',
-        },
-        cursor: 'cjq0qx0v800050981rha92qsd',
-      },
-    ],
-    aggregate: aggregateMock,
-  }));
-
-  prisma.talk = jest.fn(() => ({
-    id: 'cjq0qx0v800050981rha92aq1',
-    title: 'Prisma: ORM to rule em all',
-    slug: 'prisma-orm-to-rule-em-all',
-    previewImage: 'https://google.com',
-    length: 2500,
-    isFeatured: true,
-    createdAt: '2018-12-23T10:24:29.259Z',
-    updatedAt: '2018-12-23T10:24:29.259Z',
-    speaker: speakerMock,
-    tags: tagsMock,
-  }));
-
   test('talkConnection pagination', async () => {
     const response = await graphqlTestCall(/* GraphQL */ `
-    {
-      talkConnection(orderBy:createdAt_DESC) {
-        pageInfo {
-          hasNextPage
-          hasPreviousPage
-          startCursor
-          endCursor
-        }
-        edges {
-          node {
-            id
-            title
-            length
-            speaker{
-              id
-              name
-            }
+      {
+        talkConnection(orderBy: createdAt_DESC) {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            endCursor
           }
-          cursor
-        }
-        aggregate {
-          count
+          edges {
+            node {
+              id
+              title
+              length
+              speaker {
+                id
+                name
+              }
+            }
+            cursor
+          }
+          aggregate {
+            count
+          }
         }
       }
-    }
-  `);
+    `);
     expect(prisma.talksConnection).toHaveBeenCalledWith({
       orderBy: 'createdAt_DESC',
     });
@@ -84,31 +45,31 @@ describe('Talk Resolvers', () => {
   test('talkBySlug', async () => {
     const response = await graphqlTestCall(/* GraphQL */ `
       {
-  talkBySlug(slug: "prisma-orm-to-rule-em-all") {
-    id
-    title
-    slug
-    previewImage
-    length
-    isFeatured
-    createdAt
-    updatedAt
-    tags{
-      name
-    }
-    speaker {
-      id
-      name
-      username
-      email
-      githubToken
-      profilePic
-      createdAt
-      updatedAt
-    }
-  }
-}
-  `);
+        talkBySlug(slug: "prisma-orm-to-rule-em-all") {
+          id
+          title
+          slug
+          previewImage
+          length
+          isFeatured
+          createdAt
+          updatedAt
+          tags {
+            name
+          }
+          speaker {
+            id
+            name
+            username
+            email
+            githubToken
+            profilePic
+            createdAt
+            updatedAt
+          }
+        }
+      }
+    `);
     expect(prisma.talk).toHaveBeenCalledWith({
       slug: 'prisma-orm-to-rule-em-all',
     });

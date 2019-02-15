f270f8ed9414153b9798d47cac3f815925ebff49
Harshit Pant <pantharshit00@gmail.com>
2019-02-16 00:08:25 +0530

    feat: init repo hero

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NewsCard } from '../components/Card';
+import { NewsCard, Card } from '../components/Card';
 import styled from '../lib/styled-components';
 import { Typography } from '../components/Typography';
 import { MONO_FAMILY } from '../components/shared';
@@ -7,7 +7,7 @@ import { MONO_FAMILY } from '../components/shared';
 const HeroTop = styled.section`
   display: grid;
   grid-gap: 2rem;
-  margin: 2rem 0;
+  margin: 2rem 0 5rem 0;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: repeat(3, 1fr);
   grid-template-areas: 'featured item1' 'featured item2' 'featured item3';
@@ -29,6 +29,15 @@ const HeroTop = styled.section`
     grid-template-rows: 3fr repeat(3, minmax(15vh, 1fr));
     grid-template-areas: 'featured' 'item2' 'item3' 'item1';
   }
+`;
+
+const RepoHero = styled.div`
+  display: grid;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+  grid-template-rows: 1fr;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
 `;
 
 export default () => (
@@ -55,5 +64,40 @@ export default () => (
         tags={['react', 'suspense']}
       />
     </HeroTop>
+    <Typography fontFamily={MONO_FAMILY} fontSize="24px" m={2}>
+      🔥 Trending Repos
+    </Typography>
+    <RepoHero>
+      <Card
+        tags={['react', 'testing']}
+        content="Simple and complete react dom testing utilities that encourage good testing practice"
+        heading="react-testing-library"
+      />
+      <Card
+        tags={['graphql', 'server']}
+        content="A lightweight 'Ruby on Rails'-like framework for GraphQL"
+        heading="yoga"
+      />
+      <Card
+        tags={['react', 'css-in-js']}
+        content="CSS-in-JS library designed for high performance style composition"
+        heading="emotion"
+      />
+      <Card
+        tags={['graphql', 'resolver-first']}
+        content="GraphQL Nexus: Code-First, Type-Safe, GraphQL Schema Construction "
+        heading="nexus"
+      />
+      <Card
+        tags={['webcomponents', 'compiler']}
+        content="A Web Component compiler for building fast, reusable UI components and Progressive Web Apps 💎 Built by the Ionic Framework team "
+        heading="stencil"
+      />
+      <Card
+        tags={['react', 'testing']}
+        content="React Native for Web"
+        heading="react-native-web"
+      />
+    </RepoHero>
   </>
 );

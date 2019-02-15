646417c0f329f07b877d754a9cedee7abb9b336c
Harshit Pant <pantharshit00@gmail.com>
2019-02-15 19:38:51 +0530

    feat: fix header's css

--- packages/frontend/components/Header/Header.tsx
+++ packages/frontend/components/Header/Header.tsx
@@ -7,29 +7,32 @@ import { Button } from '../Button';
 
 export const StyledHeader = styled.header<BoxShadowProps & SpaceProps>`
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.16);
-  padding: 0.5rem 2rem 0 2rem;
   border-top: 4px solid ${theme.colors.purple};
-  display: flex;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-rows: 1fr;
+  padding-left: 1rem;
+  padding-right: 1rem;
   ${boxShadow}
   ${space}
 `;
 
 export const Navlink = styled.a<{ active?: boolean }>`
-  padding: 0 0 16px 0;
   cursor: pointer;
   color: black;
   font-family: ${MONO_FAMILY};
   font-size: 24px;
   margin: 0 1rem;
+  display: inline-flex;
+  align-items: center;
+  height: 100%;
   &:hover {
     font-weight: bold;
   }
   ${props =>
     props.active &&
     css`
-      border-bottom: 3px solid ${theme.colors.purple};
-      font-weight: 600;
+      box-shadow: ${p => p.theme.colors.purple} 0px -4px 0px inset;
     `}
 `;
 
@@ -38,36 +41,33 @@ export interface Props extends BoxShadowProps, SpaceProps {}
 export const Header: React.FC<Props> = props => {
   return (
     <StyledHeader {...props}>
-      <div
-        css={`
-          margin-top: 5px;
-        `}
-      >
+      <div>
         <Navlink active>Home</Navlink>
         <Navlink>Repos</Navlink>
         <Navlink>News</Navlink>
         <Navlink>Talks</Navlink>
       </div>
-      <div>
-        <JsafLogoBlack />
+      <div
+        css={`
+          padding: 0.6rem;
+        `}
+      >
+        <div
+          css={`
+            position: relative;
+            left: 37.7777%;
+          `}
+        >
+          <JsafLogoBlack />
+        </div>
       </div>
       <div
         css={`
           display: flex;
-          padding: 0rem 0rem 0.6rem 0rem;
           justify-content: center;
         `}
       >
-        <Button
-          fontFamily={MONO_FAMILY}
-          fontSize={'22px'}
-          mx={3}
-          my={0}
-          variant="icon"
-        >
-          Login
-        </Button>
-        <Button fontFamily={MONO_FAMILY} fontSize={'22px'} px={3} py={0} m={0}>
+        <Button fontSize={'20px'} mx={3} my={0} variant="icon">
           Sign In
         </Button>
       </div>
--- packages/frontend/components/shared/constants.ts
+++ packages/frontend/components/shared/constants.ts
@@ -1,2 +1,2 @@
-export const SANS_FAMILY = ` 'PT Sans', sans-serif`;
+export const SANS_FAMILY = `-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif`;
 export const MONO_FAMILY = ` 'Inconsolata', sans-serif`;
